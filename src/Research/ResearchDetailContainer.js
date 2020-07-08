720467c54303cae4c066658f6fda1e70f3b0e509
epittab <ep7m@me.com>
2020-07-08 17:08:49 -0400

    added graph

--- src/Research/ResearchDetailContainer.js
+++ src/Research/ResearchDetailContainer.js
@@ -1,18 +1,24 @@
 import React from 'react'
 
 import {Switch, Route} from 'react-router-dom'
+import {
+    BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
+  } from 'recharts';
 
 import JobListingDetail from './JobListingDetail'
 
 import './ResearchDetailContainer.css'
 import JobListingShowPage from './JobListingShowPage'
 
+
+
 class ResearchDetailContainer extends React.Component {
 
     constructor() {
         super()
         this.state = {
-            listings: []
+            listings: [],
+            showingGraph: false
         }
     }
 
@@ -32,15 +38,83 @@ class ResearchDetailContainer extends React.Component {
         })
     }
 
+    handleToggleGraph = () => {
+        this.setState({showingGraph: !this.state.showingGraph})
+    }
+
+    cleanListingsData() {
+        let listingObj = {}
+
+        this.state.listings.forEach(l => {
+
+            if (listingObj[l.city])
+               { listingObj[l.city] += 1}
+            else
+               { listingObj[l.city] = 1}
+
+
+        })
+
+        let list_array = Object.entries(listingObj).map( l => {
+            return {city: l[0], count: l[1] }
+        } )
+
+        return list_array
+    }
+    
+
     render(){
+        let listingsData = this.cleanListingsData()
+        console.log(listingsData)
         return(
 
             <Switch>
 
                 <Route exact path='/research' render= {() => {
-                    return (<div className='ResearchDetailContainer'>
+                    return (
+
+                        <div>
+
+                            <div className='Research-toggle-button'>
+                                <div className={`toggle-button ${ this.state.showingGraph ? '' : 'active'}`} onClick={this.handleToggleGraph}>List</div>
+                                <div className={`toggle-button ${ this.state.showingGraph ? 'active' : ''}`} onClick={this.handleToggleGraph}>Graph</div>
+                            </div>
+                        
+                            {this.state.showingGraph ? 
+                            
+                            <div className='ResearchDetailContainer'>
+                                <BarChart
+                                    width={500}
+                                    height={300}
+                                    data={listingsData}
+                                    margin={{
+                                    top: 5, right: 30, left: 20, bottom: 5,
+                                    }}
+                                >
+                                    <CartesianGrid strokeDasharray="3 3" />
+                                    <XAxis dataKey="city" />
+                                    <YAxis />
+                                    <Tooltip />
+                                    <Legend />
+                                    <Bar dataKey="count" fill="#8884d8" />
+                                    
+                                </BarChart>
+                            </div>
+                            
+                            :
+
+                             <div className='ResearchDetailContainer'>
                                 { this.state.listings.map( jl => <JobListingDetail key={jl.id} listing={jl}/> )}
-                            </div>)
+                            </div>
+                            }
+                        
+                        
+                        </div>
+                        
+                    
+                           
+                            
+                            )
                 }} />
 
             <Route exact path='/research/:id' render = { (props) =>  < JobListingShowPage listings={this.state.listings} id={props.match.params.id}/>} />
